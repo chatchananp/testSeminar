0d8ac042a8fe24eec418d27bae3d1a0e4c2c394f
LAPTOP-3KA930G9\baschatt <baschattkrab@gmail.com>
2020-08-31 16:09:44 +0700

    tranfer all method about time calculation in "Item" file to the new file "Time"

---
+++ src/knapsack/Time.java
@@ -0,0 +1,63 @@
+package knapsack;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Time {
+	int minute = 0;
+	int calHour = 0;
+	int calMinute = 0;
+	int startHour = 9;
+	int startMinute = 0;
+	int n = 1;
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mma");
+	
+	public LocalTime seminarTime(int startH, int startM) {
+		LocalTime Time = LocalTime.of(startH, startM);
+		return Time;
+	}
+	
+	public int setHour(int hr) {
+		startHour = hr;
+		return startHour;
+	}
+	
+	public int setMinute(int min) {
+		startMinute = min;
+		return startMinute;
+	}
+	
+	public int calHour(int userMinute) {
+		LocalTime Time = LocalTime.of(startHour, startMinute);
+		int newHour = (userMinute + startMinute) / 60;
+		calHour = Time.getHour() + newHour;
+		startHour = calHour;
+		return calHour;
+	}
+	
+	public int calMinute(int userMinute) {
+		calMinute = (userMinute + startMinute) % 60;
+		startMinute = calMinute;
+		return calMinute;
+	}
+
+	public LocalTime BeginTime(int userMinute) {
+		LocalTime BeginTime = seminarTime(startHour, startMinute);
+		return BeginTime;
+	}
+	
+	public LocalTime EndTime(int userMinute) {
+		int endHr = calHour(userMinute);
+		int endMin = calMinute(userMinute);
+		LocalTime EndTime = seminarTime(endHr, endMin);
+		return EndTime;
+	}
+	
+	public String normalTime (int userMinute) {
+		LocalTime beginT = BeginTime(userMinute);
+		EndTime(userMinute);
+
+		String beginTime = beginT.format(dtf); //+ EndTime(userMinute);
+		return beginTime;
+	}
+}
